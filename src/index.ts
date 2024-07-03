3dee5dafaf67a7173f110900b45ce1f07af41da0
OrregoDev <camilo.orrego554@gmail.com>
2024-07-03 07:37:56 -0500

    crete typescript structure and apply to the web page

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+  console.log(
+    `${type.value} is from: ${toFrom.value} details: ${details.value} the amount is: ${amount.valueAsNumber}`
+  );
+});
